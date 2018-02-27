921ee7166604c93aa7e3cc70c3fdfe7846f08f15
shagr4th <shagr4th@gmail.com>
2018-02-27 22:00:21 +0100

    testing datatable and angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
-
+import {DataTableModule} from '../datatable';
 import { AppComponent } from './app.component';
 
+import {Demo1} from './demo1/demo1';
+import {Demo2} from './demo2/demo2';
+import {Demo3} from './demo3/demo3';
+import {Demo4} from './demo4/demo4';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Demo1,
+    Demo2,
+    Demo3,
+    Demo4
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    DataTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
